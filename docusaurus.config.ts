70650cad80f16339e205885afe42444897537adc
doc769 <asing.cn@gmail.com>
2026-03-07 21:29:01 +0800

    i18n

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,7 +75,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/intro',
             },
           ],
         },
