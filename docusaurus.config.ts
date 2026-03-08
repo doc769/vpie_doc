f3f8cb1a84cddc0803ab1144104ae8e106c22bf2
doc769 <asing.cn@gmail.com>
2026-03-08 16:36:08 +0800

    i18n

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -30,7 +30,7 @@ const config: Config = {
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
-  trailingSlash: true,
+  trailingSlash: false,
   i18n: {
     defaultLocale: 'zh',
     locales: ['zh', 'en', 'ja', 'ko'],
